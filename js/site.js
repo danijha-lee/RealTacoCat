bcf712ae486bf10b635c841869b7733eff74c7c2
danijha-lee <84164688+danijha-lee@users.noreply.github.com>
2021-07-22 14:41:24 -0400

    2 errors on js

---
+++ js/site.js
@@ -0,0 +1,28 @@
+function getValues() {
+
+    document.getElementById("alert").classList.add("invisible");
+    let userString = document.getElementById("userString").value;
+    
+    let revString = reverseString(userString);
+    
+    displayString(revString);
+}
+
+function reverseString(userString) {
+    let revString = [];
+
+    for (let index = userString.length - 1; index >= 0; index--) {
+        revString += userString[index];
+        
+    }
+    
+    return revString;
+}
+
+funciton displayString(revString);
+{
+    document.getElementById("msg").innerHTML = `Your string reversed is: ${revString}`;
+    
+    document.getElementById("alert").classList.remove("invisible");
+
+}
