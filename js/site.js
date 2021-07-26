30278c92b46b1d164403e347b6b6ee9386895ed9
danijha-lee <84164688+danijha-lee@users.noreply.github.com>
2021-07-26 15:54:06 -0400

    working js

--- js/site.js
+++ js/site.js
@@ -1,33 +1,65 @@
-function getValue  () {
+function getWord() {
+    //access the webpage  and gets the value from the inputs
+    let word = document.getElementById("word").value;
 
-    document.getElementById("alert").classList.add("invisible");
-    let userString = document.getElementById("userString").value;
-    
-    let revString = reverseString(userString);
-    
-    displayString(revString);
-}
+    //takes away capitilization, special characters and spaces from the word
+    let modifiedWord = word.toLowerCase().replace(/[^a-zA-Z0-9]/g, "");
 
-function reverseString(userString) {
-    let revString = [];
+    //takes the word and flips it and sets it to a value
+    let revWord = flipWord(modifiedWord);
 
-    for (let index = userString.length - 1; index >= 0; index--) {
-        revString += userString[index];
+    //setting up boolean and an empty string for error codes
+    let errorState = false;
+    let errorMsg = "";
+
+    //this becomes true if the word and it's palindrome doesn't equal
+    if (revWord != modifiedWord) {
+        errorState = true;
+        errorMsg += "This word is not a palindrome<hr/>"
+        errorMsg += "Your word reversed is " + revWord
         
     }
-    
-    return revString;
+
+    //this becomes true if they didn't put enough letters to have a palindrome
+    if (modifiedWord.length < 2) {
+        errorState = true;
+        errorMsg += "You didn't add enough letters for this."
+    }
+
+    //if any of the previous becomes true, a SweetAlert will fire and exit the function
+    if (errorState) {
+        Swal.fire(
+            'Something went wrong',
+            `${errorMsg}`,
+            'error'
+        )
+        return;
+    }
+
+    //take the word and it's plaindrome and pass it to a function that will display it
+    displayResults(word, revWord);
 }
 
-funciton displayString(revString);
-{
-    document.getElementById("msg").innerHTML = `Your string reversed is: ${revString}`;
-    
-    document.getElementById("alert").classList.remove("invisible");
+//function that takes the value and make a string that is the word reversed
+function flipWord(word) {
 
+    //creates an empty string
+    let revWord = "";
+
+    //for every letter in the word, create a word with the letters in reverse
+    for (let index = word.length - 1; index >= 0; index--) {
+        let letter = word[index];
+
+        revWord += letter;
+    }
+
+    return revWord;
 }
 
-for (let index = 0; index < array.length; index++) {
-    const element = array[index];
-    
+//take the words and display the words to the webpage
+function displayResults(word, revWord) {
+    let output = document.getElementById("output");
+
+    output.innerText = `Congrats! This is a palindrome!
+    You entered the word ${word}. Reversed is ${revWord}.`
 }
